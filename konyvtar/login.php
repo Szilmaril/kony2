100091e4027a67c57c0b91534417304687c4b186
Kek Elek <petho.9884@gmail.com>
2019-03-17 23:29:45 +0100

    login oldal

--- konyvtar/login.php
+++ konyvtar/login.php
@@ -27,30 +27,55 @@ if(isset($_POST["login"])){
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-		<div class="container" style="margin-top: 5%;">
-			<div class="text-center">
-				<form action="" method="POST">
-			<?php //require_once "errorMessage.php"; ?>
-			<label for="username">Username </label>
-			<input type="text" id="username" name="username">
-			<br>
-			<label for="password">Jelszó</label>
-			<input type="password" id="password" name="password">
-			<br>
-			<button name="login" class="btn btn-secondary">
-				Bejelentkezés
-			</button><br>
-			<!--<a href="#" style="color: black; text-decoration: none;">
-				Elfelejtettem a jelszavam!
-			</a>-->
-		</form>
-			</div>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/login.css">
+	<style>
+		html, body {
+			overflow: hidden;
+		}
+
+	</style>
+</head>
+<body background="image/login.jpg">
+	<div class="container" style="margin-top: 5%;">
+		<div class="text-center">
+			<form action="" method="POST">
+				<div class='box2'>
+					<img src='image/login.jpg' style="opacity: 0;">
+					<div class='box-content'>
+						<div class='inner-content'>
+							<h3 class='title text-center'>Bejelentkezes</h3>
+							<span class='post'>
+								<div class='container text-center'>
+									<div class='form-roup'>
+										<form method='POST'>
+											<div class='form-row'>
+												<div class='col'>
+													<input type='text' name='username' id="username" class='registry' placeholder='username' required="true">
+													<input class='registry' type='password' id="password" name='password' placeholder='jelszo' required="true">
+												</div>		
+											</div>
+											<div class='form-row'>
+												<div class='col'>
+													<br>
+													<button class='btn btn-primary' name='login' >Belepes</button>
+													<button class='btn btn-danger' name='login' onclick="window.location.href='index.php'">Vissza</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</span>
+						</div>
+					</div>
+				</div>
+			</form>
 		</div>
-		
-	</body>
+	</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 </html>
