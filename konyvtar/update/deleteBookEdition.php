c76d04b001b5108212adfeacf0ee8420c89a3c64
Kek Elek <petho.9884@gmail.com>
2019-03-20 20:42:41 +0100

    Update deleteBookEdition.php

--- konyvtar/update/deleteBookEdition.php
+++ konyvtar/update/deleteBookEdition.php
@@ -1,4 +1,25 @@
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h2>Biztos?</h2>
+			<small>A konyv orokre letorlodik. (Ez eleg hosszu ido..)</small>
+		</div>
+		<form method="post">
+			<button name="delete" class="btn btn-danger">PUSZTULJON!!</button>
+			<a href="listBook.php" class="btn btn-primary">NEEEEEEEEEEEEEEE.....</a>
+		</form>
+	</div>
+</body>
+</html>
 <?php 
+if (isset($_POST["delete"])) {
 	if(isset($_GET["booksid"])){
 		require_once "../db.php";
 		$db = db::get();
@@ -9,4 +30,5 @@
 	}else{
 		header("listBooks.php");
 	}
+}
 ?>
